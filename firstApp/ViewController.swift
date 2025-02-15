175cf19448e739ff237dfdd645d2fa72f11c210b
Anoop kumar <kanoop699@gmail.com>
2025-02-15 09:55:50 +0530

    Initial Commit

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by Class on 15/02/25.
+//
+
+import UIKit
+import Foundation
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        firstClassResponse()
+    }
+    
+    func firstClassResponse() {
+        print("Anoop this side")
+    }
+
+}
+

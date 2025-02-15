9588952e51f85a0e21b5ca2e124cc269e2d900f2
Anoop kumar <kanoop699@gmail.com>
2025-02-15 11:28:25 +0530

    UI created

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -9,21 +9,52 @@ import UIKit
 import Foundation
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var firstLabl: UILabel!
+    @IBOutlet weak var userTextfield: UITextField!
+    @IBOutlet weak var passwrdTextfield: UITextField!
+    var alearMsg: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
-        firstClassResponse()
+        view.backgroundColor = .systemBackground
+        passwrdTextfield.delegate = self
+        userTextfield.delegate = self
     }
     
-    func firstClassResponse() {
-        print("Anoop this side")
-        print("Anoop works as assistant professor in TMU")
-        print("Anoop is not a common man, he is a desrving candidate that can win anything in his life")
+    
+    @IBAction func submitBtn(_ sender: UIButton) {
+        if userTextfield.text?.isEmpty == true || passwrdTextfield.text?.isEmpty == true {
+            aleartMessage(message: "Failed")
+        } else {
+            aleartMessage(message: "Success")
+        }
+        
     }
     
-
+    func aleartMessage(message: String) {
+            aleartPopUp(aleartPop: message)
+        }
+    
+    func aleartPopUp(aleartPop: String) {
+        let alert = UIAlertController(title: "Alert", message: "\(aleartPop)", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        passwrdTextfield.text = ""
+//        userTextfield.text = ""
+//    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if userTextfield.text?.isEmpty == false && passwrdTextfield.text?.isEmpty != true {
+            userTextfield.text = ""
+            passwrdTextfield.text = ""
+        }
+    }
+    
 }
 
